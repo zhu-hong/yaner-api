69b95366034efe93e6ea8d44300bb6d14b571bbb
zhu-hong <zhxhyy@qq.com>
2021-10-16 16:45:53 +0800

    refactor statusCode

--- status/index.js
+++ status/index.js
@@ -1,47 +1,49 @@
-const [SUCCESS, ERROR] = [200, 500]
-
-// 账号错误
-const [
-  ERROR_ACCOUNT_NOT_EXIST,
-  ERROR_PASSWORD_WRONG,
-  ERROR_ACCOUNT_SCOPELESS,
-] = [
-    30001,
-    30002,
-    30003,
-]
-
-// Token错误
-const [
-  ERROR_TOKEN_TIMEOUT,
-  ERROR_TOKEN_WORNG,
-] = [
-    40001,
-    40002
-]
-
 const codemsg = new Map()
-
-codemsg.set(200, 'OK')
-codemsg.set(500, 'FAIL')
-codemsg.set(30001, '账号不存在')
-codemsg.set(30002, '账号或密码错误')
-codemsg.set(40001, 'TOKEN TIMEOUT')
-codemsg.set(40002, 'TOKEN WORNG')
-codemsg.set(30003, '权限不足')
 
 const getCodeMsg = (code) => {
   const message = codemsg.get(code)
   return message
 }
 
-module.exports = {
+// 不导致账号退出
+const [
   SUCCESS,
-  ERROR,
+  ERROR_ACCOUNT_SCOPELESS,
+] = [
+  200,
+  301,
+]
+codemsg.set(200, 'OK')
+codemsg.set(301, '权限不足')
+
+// 账号错误
+const [
   ERROR_ACCOUNT_NOT_EXIST,
   ERROR_PASSWORD_WRONG,
-  getCodeMsg,
+] = [
+    400,
+    401,
+]
+codemsg.set(400, '账号不存在')
+codemsg.set(401, '账号或密码错误')
+
+// Token错误
+const [
   ERROR_TOKEN_TIMEOUT,
   ERROR_TOKEN_WORNG,
+] = [
+    500,
+    501,
+]
+codemsg.set(500, 'TOKEN TIMEOUT')
+codemsg.set(501, 'TOKEN WORNG')
+
+module.exports = {
+  getCodeMsg,
+  SUCCESS,
   ERROR_ACCOUNT_SCOPELESS,
+  ERROR_ACCOUNT_NOT_EXIST,
+  ERROR_PASSWORD_WRONG,
+  ERROR_TOKEN_TIMEOUT,
+  ERROR_TOKEN_WORNG,
 }
